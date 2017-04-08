b2cea856ffe352ed38af1e9144d8a5d446f604e7
MoonlightCapital <lop-2013@libero.it>
2017-04-08 19:25:12 +0200

    Update footer stuff

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@ else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofo
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by <a href="http://moonlightcapital.h05t.gq/abxd/">AcmlmBoard XD</a> 3.1<br />
-By Dirbaio, xfix, Kawa, StapleButter, Nina, Super-toad 65 et al<br />
+Powered by <b>AcmlmBoard XD</b> 3.1.1<br />
+By Dirbaio, xfix, Kawa, StapleButter, Nina, MoonlightCapital et al<br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
